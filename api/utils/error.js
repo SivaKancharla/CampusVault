af93f2c0ff5a4a3b7452cb4f8ec408b21f6081e8
SivaKancharla <23je0452@iitism.ac.in>
2026-08-21 23:33:03 +0530

    spelling corrected and exported correctly

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,8 @@
-const errrorHandler=(statusCode,message)=>{
+//This is used to create differnt types of errors and return them
+export const errorHandler=(statusCode,message)=>{
     const error=new Error();
     error.statusCode=statusCode;
     error.message=message;
     return error;
 };
 
-export default errrorHandler
