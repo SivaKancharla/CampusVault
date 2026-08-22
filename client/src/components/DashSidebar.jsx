efa2b397711119f55bd22252bc186babf8f8b421
SivaKancharla <23je0452@iitism.ac.in>
2026-08-22 18:52:17 +0530

    change SidebarItem tag into a Link

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import { Sidebar,SidebarItems,SidebarItemGroup,SidebarItem } from 'flowbite-react';
-import {
-  HiUser,
-  HiArrowSmRight,
-  HiDocumentText,
-  HiOutlineUserGroup,
-  HiAnnotation,
-  HiChartPie,
-} from 'react-icons/hi';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Sidebar, SidebarItems, SidebarItemGroup, SidebarItem } from 'flowbite-react';
+import { HiArrowSmRight, HiChartPie } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function DashSidebar() {
-    const location = useLocation();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState('');
+  
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -24,15 +17,22 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
   return (
      <Sidebar className='w-full md:w-56'>
         <SidebarItems>
-             <SidebarItemGroup >
-                <Link to='/dashboard?tab=profile'>
-                <SidebarItem active={tab==='profile'} icon={HiChartPie} label={'User'} labelColor='dark'> 
+             <SidebarItemGroup>
+                {/* FIX: Replaced wrapper <Link> with as={Link} directly on SidebarItem */}
+                <SidebarItem 
+                  as={Link} 
+                  to='/dashboard?tab=profile' 
+                  active={tab === 'profile'} 
+                  icon={HiChartPie} 
+                  label={'User'} 
+                  labelColor='dark'
+                > 
                     Profile
                 </SidebarItem>
-                </Link>
                 <SidebarItem icon={HiArrowSmRight} className='cursor-pointer'> 
                     Sign Out
                 </SidebarItem>
