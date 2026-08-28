ecb488564707f8b278df1871cbb3024487fa2a3a
SivaKancharla <23je0452@iitism.ac.in>
2026-08-29 01:21:43 +0530

    complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(
+        `/api/post/getPosts`
+      );
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
-      You are in Home
+      <div className='flex flex-col gap-6 p-10  px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl pt-10'>
+          Welcome to CampusVault
+        </h1>
+
+        <p className='text-gray-500 text-xs sm:text-sm max-w-2xl'>
+          CampusVault is a platform where students can discover and share
+          valuable knowledge, experiences, and insights. Explore posts about
+          academics, coding, internships, placements, campus life, and much more.
+          Learn from others, share your experiences, and help build a growing
+          knowledge community.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all posts
+        </Link>
+         <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+          <CallToAction />
+        </div> 
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-3'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
